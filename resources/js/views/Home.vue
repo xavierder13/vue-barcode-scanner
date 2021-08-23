e4a81b7583b5594fd2f06835a8e173c50bc0ff1d
Xavier De Guzman <xavierder26@gmail.com>
2021-08-23 11:29:25 +0800

    modify vueroles on User Index page

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -147,6 +147,12 @@
     </v-overlay>
     <!-- Content -->
     <router-view />
+    <v-footer padless dense dark app>
+      <v-col class="text-center" cols="12">
+        Copyright © {{ new Date().getFullYear() }} —
+        <strong> LARAVEL VUE BARCODE SCANNER</strong>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
